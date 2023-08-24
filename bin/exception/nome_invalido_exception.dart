ab78419e3854b1973b120527eb5d2862eb9082a0
Fernanda <prodigpropri@gmail.com>
2023-08-24 00:05:25 -0300

    praticando a aula - "Criando Exceções Customizadas"

---
+++ bin/exception/nome_invalido_exception.dart
@@ -0,0 +1,7 @@
+class NomeInvalidoException implements Exception {
+  String error() => "Nome Inválido";
+  @override
+  String toString() {
+    return "NomeInvalidoException: ${error()}\n";
+  }
+}
--- bin/firt.dart
+++ bin/firt.dart
@@ -2,10 +2,20 @@ import 'dart:core';
 
 import 'models/aluno.dart';
 import 'models/console_utils.dart';
+import 'exception/nome_invalido_exception.dart';
 
 void main() {
   print("Bem vindo ao sistema de notas!");
-  String inserirNomeAluno = ConsoleUtils.lerStringComTexto("Informe nome do aluno: ");
+  String inserirNomeAluno =
+      ConsoleUtils.lerStringComTexto("Informe nome do aluno: ");
+  try {
+    if (inserirNomeAluno.trim() == "") {
+      throw NomeInvalidoException();
+    }
+  } catch (NomeInvalidoException) {
+    print(NomeInvalidoException);
+  }
+
   var aluno = Aluno(inserirNomeAluno);
   lerNotasAluno(aluno);
 }
@@ -14,13 +24,13 @@ lerNotasAluno(Aluno aluno) {
   double? nota;
   do {
     nota = ConsoleUtils.lerDoubleComSaida("Digite nota ou S para sair: ", "S");
-    if(nota != null){
+    if (nota != null) {
       aluno.addNotas(nota);
     }
-  } while(nota != null);
+  } while (nota != null);
   print(aluno.getNotas());
   print("A Média do aluno ${aluno.getNome()} foi: ${aluno.retornaMedia()}");
-  if(aluno.aprovado(7)) {
+  if (aluno.aprovado(7)) {
     print("${aluno.getNome()} você foi APROVADO!");
   } else {
     print("${aluno.getNome()} você foi REPROVADO!");
